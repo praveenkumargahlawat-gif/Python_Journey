fb8b5bc48ea585d11609908b1f593d1be93d82a2
praveenkumargahlawat-gif <praveenkumargahlawat@gmail.com>
2025-09-22 12:01:00 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,1 +1,13 @@
 print('mission 10cr - Day 1')
+# Day 1
+
+### Programs Completed
+- Hello Soldier (first print)
+- Simple Interest Calculator
+- Prime Number Check
+
+### Key Learnings
+- How to run Python scripts
+- Importance of indentation
+- Using boolean values (True/False)
+- First git commit & push
